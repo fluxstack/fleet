6d4edf37b933dbb2ffde72bd188bec6c0258556b
qiulin <qlin.dev@outlook.com>
2025-03-08 00:27:58 +0800

    update

--- jsonapi/responder.go
+++ jsonapi/responder.go
@@ -1,3 +1,14 @@
 package jsonapi
 
+import "github.com/lynx-go/fastapi/apierror"
+
 type M map[string]interface{}
+
+type Response[T any] struct {
+	apierror.APIError
+	Data T `json:"data"`
+}
+
+func NewResponse[T any](data T) *Response[T] {
+	return &Response[T]{}
+}
