87957b896e4a7c25f0397919f67ee41ccb5d8c28
qiulin <qlin.dev@outlook.com>
2025-03-08 15:02:54 +0800

    update: response

--- jsonapi/responder.go
+++ jsonapi/responder.go
@@ -11,6 +11,32 @@ type Response[T any] struct {
 	Data T `json:"data"`
 }
 
-func NewResponse[T any](data T) *Response[T] {
-	return &Response[T]{}
+func OK[T any](data T) *Response[T] {
+	return &Response[T]{
+		APIError: errors.APIError{
+			Message: "OK",
+		},
+		Data: data,
+	}
+}
+
+func ERR[T any](err error, datas ...T) *Response[T] {
+	var data T
+	if len(datas) > 0 {
+		data = datas[0]
+	}
+	var apiErr *errors.APIError
+	switch e := err.(type) {
+	case *errors.APIError:
+		apiErr = e
+	default:
+		apiErr = &errors.APIError{
+			Code:    -1,
+			Message: e.Error(),
+		}
+	}
+	return &Response[T]{
+		APIError: *apiErr,
+		Data:     data,
+	}
 }
