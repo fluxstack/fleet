c6272d295a920d40c1d52380418409b354326250
qiulin <qlin.dev@outlook.com>
2025-03-08 11:24:01 +0800

    update: pubsub

--- pubsub/broker.go
+++ pubsub/broker.go
@@ -1,7 +1,30 @@
 package pubsub
 
-import "gocloud.dev/pubsub"
+import (
+	"gocloud.dev/pubsub"
+	"strings"
+)
 
 type Broker struct {
 	clients map[string]*pubsub.Topic
+}
+
+type TopicURI string
+
+func (uri TopicURI) String() string {
+	return string(uri)
+}
+
+func (uri TopicURI) Namespace() string {
+	uris := strings.Split(string(uri), ":")
+	return uris[0]
+}
+
+func (uri TopicURI) Topic() string {
+	uris := strings.Split(string(uri), ":")
+	return uris[len(uris)-1]
+}
+
+func NewTopicURI(namespace string, topic string) TopicURI {
+	return TopicURI(namespace + ":" + topic)
 }
