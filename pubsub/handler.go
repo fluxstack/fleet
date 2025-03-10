4d084217bdef30719491b5a7c9dca451c1ca2c43
Qiulin Li <liqiulin@outlook.com>
2025-03-10 19:32:12 +0800

    update: pubsub

--- pubsub/handler.go
+++ pubsub/handler.go
@@ -1,5 +1,18 @@
 package pubsub
 
-import "context"
+import (
+	"context"
+	"github.com/lynx-go/x/json"
+)
 
 type HandlerFunc func(ctx context.Context, event []byte) error
+
+func H[E any](h func(ctx context.Context, e E) error) HandlerFunc {
+	return func(ctx context.Context, event []byte) error {
+		var e E
+		if err := json.Unmarshal(event, &e); err != nil {
+			return err
+		}
+		return h(ctx, e)
+	}
+}
